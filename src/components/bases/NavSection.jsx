c1b282549fba05cbd8aa644cf101b60a0cb5c0c5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 11:55:00 +0800

    Update NavSection.jsx

--- src/components/bases/NavSection.jsx
+++ src/components/bases/NavSection.jsx
@@ -1,16 +1,42 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
 import styled from 'styled-components';
 import Section from './Section';
 
 const NavSection = styled(Section)`
-	position: relative;
-	overflow: hidden;
-	-webkit-flex-direction: row-reverse;
-	flex-direction: row-reverse;
-
+  position: relative;
+  overflow: hidden;
+  -webkit-flex-direction: row-reverse;
+  flex-direction: row-reverse;
 
   @media screen and (max-width: 50em) {
-		padding: 4em 0;
-	}
+    padding: 4em 0;
+  }
 `;
 
 export default NavSection;
