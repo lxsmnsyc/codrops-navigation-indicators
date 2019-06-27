19947d77e35c42e759edcba1d13a9c6c61339d9a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 17:15:22 +0800

    Create DestaNavItem.jsx

---
+++ src/components/nav/desta/DestaNavItem.jsx
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import styled from 'styled-components';
+import NavItem from '../NavItem';
+import DestaNavItemTitle from './DestaNavItemTitle';
+import DestaNavItemIcon from './DestaNavItemIcon';
+
+const DestaNavItem = styled(NavItem)`
+  width: 5em;
+  height: 3.5em;
+
+  &:not(.nav__item--current):focus ${DestaNavItemIcon},
+  &:not(.nav__item--current):hover ${DestaNavItemIcon} {
+    fill: #fff;
+  }
+
+  &.nav__item--current ${DestaNavItemIcon} {
+    -webkit-transform: rotate3d(0,0,1,0deg);
+    transform: rotate3d(0,0,1,0deg);
+    fill: #fff;
+  }
+
+  &.nav__item--current ${DestaNavItemTitle} {
+    opacity: 1;
+    -webkit-transform: translate3d(0,0,0);
+    transform: translate3d(0,0,0);
+  }
+`;
+
+export default DestaNavItem;
