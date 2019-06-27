1e621a8114e2df0e857e11a059ee99a466662ad4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 11:44:35 +0800

    Create Section.jsx

---
+++ src/components/bases/Section.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+	display: -webkit-flex;
+	display: flex;
+	-webkit-flex-direction: column;
+	flex-direction: column;
+	-webkit-justify-content: center;
+	justify-content: center;
+	-webkit-align-items: center;
+	align-items: center;
+`;
+
+export default Section;
