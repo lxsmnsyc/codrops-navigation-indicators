9639703178afd9632608de81e5f6bc0cb0629e5a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 11:44:53 +0800

    Create Intro.jsx

---
+++ src/components/sections/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IntroSection from '../bases/IntroSection';
+
+export default () => (
+  <IntroSection>
+      <p>
+        Many UI components require some kind of navigation or pagination. Today we want to explore some different styles on the example of a vertical slideshow.
+        <br />
+        <br />
+        <strong>Let's have a look.</strong>
+      </p>
+  </IntroSection>
+);
