2f41a28750ad1613b0d873a49d4b53f4a3a620de
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 15:43:00 +0800

    Create Anchor.jsx

---
+++ src/components/bases/Anchor.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Anchor = styled.a`
+  text-decoration: none;
+  color: #8bc34a;
+  outline: none;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+  }
+`;
+
+export default Anchor;
